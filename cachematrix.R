a602393d7f7b5d939a2183d2a7757bcc352b8f3c
xy999 <bshevenaugh@hwcinc.com>
2015-09-15 23:06:32 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## The "makeCacheMatrix" function creates a special "vector", which is really a list containing a function to do the following: set the value of the vector; get the value of the vector; set the value of the mean; and get the value of the mean
+#  The "makeCacheMatrix" function creates a special "vector", which is really a list containing a function to do the
+#  following: set the value of the vector; get the value of the vector; set the value of the mean; and get the value of the 
+#  mean.
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -16,7 +18,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Calculates the mean of the special "vector" created with the preceding function. But first, it checks to see if the mean has already been calculated. If so, it gets the mean from the cache and skips the computation. Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
+# Calculates the mean of the special "vector" created with the preceding function. But first, it checks to see if the
+# mean has already been calculated. If so, it gets the mean from the cache and skips the computation. Otherwise, it 
+# calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
 
 cacheSolve <- function(x, ...) {
         m <- x$getinv()
